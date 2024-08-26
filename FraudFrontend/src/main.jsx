77aebaffe686be1f85bc65e0686f970adbbb7cc5
Mirandaet <miranda.thulstrup@gmail.com>
2024-08-26 13:21:40 +0200

    deleted unnessesary files

--- FraudFrontend/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-
-function App() {
-  return (
-    <div className='bg-blue-600'>Hello</div>
-  )
-}
-
-createRoot(document.getElementById('root')).render(<App />)
--- src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="">Hello</div>
-  );
-}
-
-export default App;
