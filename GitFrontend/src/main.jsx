4a24f6c48ff295866054765f46d0029b4fc9fd80
Mirandaet <miranda.thulstrup@gmail.com>
2024-09-04 13:56:19 +0200

    added error to site

--- GitFrontend/src/main.jsx
+++ GitFrontend/src/main.jsx
@@ -74,6 +74,7 @@ function App() {
       <div className="text-center text-xl mt-10 mb-2">
         <p>Want to know if you're being scammed or spammed?</p>
         <p>Upload a pdf or paste the message below to let our AI find out!</p>
+        <p>{error}</p>
         <img className="h-52 mx-auto" src="/fishgif.gif" alt="fishgif" />
       </div>
 
